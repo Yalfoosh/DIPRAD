afc96f315abd3ed88c54452720b5a1b99b56d4da
Miljenko Šuflaj <headsouldev@gmail.com>
2022-03-10 19:02:25 +0100

    Initialize `dl`

---
+++ dl/README.md
@@ -0,0 +1,14 @@
+# Graduate thesis: Deep learning
+
+![](https://img.shields.io/github/license/Yalfoosh/DIPRAD?style=flat-square)
+
+![](https://img.shields.io/badge/Python-v3.8-blue?style=flat-square)
+
+
+## Installation
+
+This module is intended to be used in some environment. You can find out how to set up one [here](/docs/env/setup.md).
+
+First activate your environment (ex. `conda activate env`, assuming `env` is your environment name).
+
+Then install with PIP (ex. `pip install .`, assuming this folder is your working directory)
---
+++ dl/setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+def readme(path: str = f"{Path(__file__).resolve().parent}/README.md"):
+    with open(path) as f:
+        return f.read()
+
+
+setup(
+    name="diprad_dl",
+    version="0.0.1",
+    description="Graduate thesis: Deep learning",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yalfoosh/DIPRAD/tree/main/dl",
+    author="Miljenko Šuflaj",
+    author_email="ms50144@fer.hr",
+    license="Apache License 2.0",
+    packages=find_packages(
+        include=(
+            "diprad_dl",
+            "diprad_dl.*",
+        )
+    ),
+    install_requires=[
+    ],
+    python_requires=">=3.8, <=3.8",
+    include_package_data=True,
+    zip_safe=False,
+)
